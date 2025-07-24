77a61bdd4cfce73149e0ce93979c3ea1c411fc0a
Maciej <maciekjur123@gmail.com>
2025-07-24 13:13:57 +0200

    Adjust user input validation

--- traffic-light-system/web/src/main/java/org/example/dto/ValidatedCommandListDto.java
+++ traffic-light-system/web/src/main/java/org/example/dto/ValidatedCommandListDto.java
@@ -13,6 +13,7 @@ import java.util.Map;
 public class ValidatedCommandListDto {
 
     @NotEmpty
+    @NotNull
     @Valid
     private List<Map<@NotNull @NotEmpty String, @NotNull @NotEmpty String>> commands;
 
